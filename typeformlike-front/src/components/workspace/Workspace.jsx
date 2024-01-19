280f845c4001eefe95ca1383df189fc8f3e50359
unknown <andriesmathenjwa52@gmail.com>
2024-01-19 20:14:02 +0200

    quiz updates

--- typeformlike-front/src/components/workspace/Workspace.jsx
+++ typeformlike-front/src/components/workspace/Workspace.jsx
@@ -1,11 +1,12 @@
-import React, { useState, useEffect } from 'react';
-import { FaUserPlus } from 'react-icons/fa6';
-import './workspace.css';
-import { useNavigate } from 'react-router-dom';
-import useFetch from '../../hooks/useFetch';
+import React, { useState, useEffect } from "react";
+import { FaUserPlus } from "react-icons/fa6";
+import "./workspace.css";
+import { useNavigate } from "react-router-dom";
+import useFetch from "../../hooks/useFetch";
+import { BsGrid1X2 } from "react-icons/bs";
 
 const Workspace = () => {
-  const { data, loading, error } = useFetch('/questions');
+  const { data, loading, error } = useFetch("/questions");
   const navigate = useNavigate();
 
   const [uniqueFormNames, setUniqueFormNames] = useState([]);
@@ -24,7 +25,7 @@ const Workspace = () => {
   }, [data]);
 
   const handleCreateFormClick = () => {
-    navigate('/content');
+    navigate("/content");
   };
 
   const handleCardClick = (formName) => {
@@ -42,11 +43,20 @@ const Workspace = () => {
         <button className="workspaceBtn" onClick={handleCreateFormClick}>
           + Create new form
         </button>
+        <select className="dateOptions">
+          <option value="option1">Date created</option>
+          <option value="option2">Last day updated</option>
+          <option value="option3">Alphabetical</option>
+        </select>
       </div>
       <hr />
       <div className="cardContainer">
         {uniqueFormNames.map((formName, index) => (
-          <div key={index} className="card" onClick={() => handleCardClick(formName)}>
+          <div
+            key={index}
+            className="card"
+            onClick={() => handleCardClick(formName)}
+          >
             <p className="formName">{formName}</p>
           </div>
         ))}
